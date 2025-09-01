9fd6f71313aa5d3528f516f4f4f6151331c6c6a6
plxl <210124508+plxl@users.noreply.github.com>
2025-09-01 11:02:19 +0800

    ADD reload slash command to reload other commands without needing to restart the bot

---
+++ commands/reload.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs').promises;
+const path = require('node:path');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('reload')
+        .setDescription('Reloads a command.')
+        .addStringOption(option =>
+            option.setName('command')
+                .setDescription('The command to reload.')
+                .setRequired(true)),
+    async execute(interaction) {
+        const commandName = interaction.options.getString('command', true).toLowerCase();
+        const commandFile = path.join(__dirname, commandName + '.js');
+
+        try {
+            // make sure command file exists
+            await fs.access(commandFile, fs.constants.F_OK);
+            // remove from cache so it can be forcefully reloaded
+            delete require.cache[require.resolve(commandFile)];
+            const newCommand = require(commandFile);
+            interaction.client.commands.set(newCommand.data.name, newCommand);
+            await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply(`There was an error while reloading command \`${commandName}\`:\n\`${error.message}\``);
+        }
+    },
+};
