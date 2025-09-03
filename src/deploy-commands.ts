6320a107da4e9965883c1b98714fd9002c128d94
plxl <210124508+plxl@users.noreply.github.com>
2025-09-03 20:27:30 +0800

    UPDATE deploy-commands.js to TypeScript

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,19 +1,25 @@
-// source: https://discordjs.guide/creating-your-bot/command-deployment.html#guild-commands
-const { REST, Routes } = require('discord.js');
-const clientId = process.env.CLIENT_ID;
-const token = process.env.TOKEN;
-const fs = require('node:fs');
-const path = require('node:path');
+import { REST, Routes } from 'discord.js';
+import fs from 'node:fs';
+import path from 'node:path';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const commands = [];
+const clientId = process.env.CLIENT_ID!;
+const token = process.env.TOKEN!;
+if (!token) throw new Error('Discord bot token not found in .env!')
+if (!clientId) throw new Error('Discord bot client ID not found in .env!')
+
+const commands: any[] = [];
 const foldersPath = path.join(__dirname, 'commands');
 
-function loadCommandsToArray(folderPath) {
-    const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
+function loadCommandsToArray(folderPath: string) {
+    const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js') || file.endsWith('.ts'));
 
     for (const file of commandFiles) {
         const filePath = path.join(folderPath, file);
-        const command = require(filePath);
+        // Dynamic import works for both JS and TS
+        const commandModule = require(filePath);
+        const command = commandModule.default ?? commandModule;
 
         if ('data' in command && 'execute' in command) {
             commands.push(command.data.toJSON());
@@ -48,7 +54,7 @@ const rest = new REST().setToken(token);
         const data = await rest.put(
             Routes.applicationCommands(clientId),
             { body: commands },
-        );
+        ) as any[];
 
         console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     }
