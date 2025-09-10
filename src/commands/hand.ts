0cb23a76e0b546f68be033410883520ab4add776
plxl <210124508+plxl@users.noreply.github.com>
2025-09-10 22:40:27 +0800

    ADD hand.ts slash command for validating the user's current hand (debugging)

---
+++ src/commands/hand.ts
@@ -0,0 +1,27 @@
+import { ChatInputCommandInteraction, MessageFlags, SlashCommandBuilder } from "discord.js";
+import { getDeckSession } from "../lib/deckSessions";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('hand')
+        .setDescription('Shows your current hand for debugging purposes.'),
+
+    async execute(interaction: ChatInputCommandInteraction) {
+        const userId = interaction.user.id;
+        const deckSession = getDeckSession(userId);
+
+        if (!deckSession) {
+            return interaction.reply({
+                content: 'You don\'t appear to be playing right now. You can play with the `/deck` command!',
+                flags: MessageFlags.Ephemeral,
+            });
+        }
+
+        const hand = deckSession.hand;
+
+        interaction.reply({
+            content: hand.map(cwm => `- ${cwm.card}`).join('\n'),
+            flags: MessageFlags.Ephemeral,
+        });
+    },
+}
