11b7abe2630ad6b1e4368c88cf926c7d425bb218
plxl <210124508+plxl@users.noreply.github.com>
2025-10-25 19:23:00 +0800

    UPDATE Command interface with optional createInteraction method

--- src/types/Client.ts
+++ src/types/Client.ts
@@ -1,9 +1,10 @@
 import { Client, Collection } from 'discord.js';
-import type { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import type { SlashCommandBuilder, Interaction, ChatInputCommandInteraction } from 'discord.js';
 
 export interface Command {
     data: SlashCommandBuilder;
     execute(interaction: ChatInputCommandInteraction): Promise<void>;
+    createInteraction(interaction: Interaction): Promise<void>;
 }
 
 export class ClientWithCommands extends Client {
